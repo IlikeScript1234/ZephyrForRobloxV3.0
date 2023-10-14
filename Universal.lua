35efd26059c9c53e2df42a152532740501f9c03a
MaxlaserTech <104206888+IlikeScript1234@users.noreply.github.com>
2023-10-14 15:18:05 +0700

    Universal

---
+++ Universal.lua
@@ -0,0 +1,7 @@
+local Library = shared.GuiLibrary
+local Combat = shared.Combat
+local Visual = shared.Visual
+local Movement = shared.Movement
+local Exploit = shared.Exploits
+
+warn("universal not working right now")
