ac83e18ca794a2e3e63bfab7b083e3b57d7db17f
MaxlaserTech <104206888+IlikeScript1234@users.noreply.github.com>
2023-10-17 02:57:25 +0700

    Update Launcher.lua

--- Launcher.lua
+++ Launcher.lua
@@ -1,97 +1,111 @@
-    if not shared.Launcher then
-        local MakeFolder = function(name)
-            if not isfolder(name) then
-                makefolder(name)
-            end
+if not shared.Launcher then
+    local MakeFolder = function(name)
+        if not isfolder(name) then
+            makefolder(name)
         end
-        
-        local MakeFile = function(name, file)
-            if not isfile(name) then
-                writefile("Zephyr/".. name.. ".lua", file)
-            end
-        end
-
-        local RFile = function(name)
-            if isfile(name) then
-                delfile(name)
-            end
-        end
-
-        MakeFolder("Zephyr")
-        MakeFolder("Zephyr/Modules")
-        MakeFolder("Zephyr/Assets")
-        MakeFolder("Zephyr/Profiles")
-
-        local UiLibrary = "https://raw.githubusercontent.com/IlikeScript1234/ZephyrForRobloxV3.0/main/GuiLibrary.lua"
-        local GuiC = game:HttpGet(UiLibrary, true)
-        MakeFile("GuiLibrary", GuiC)
-        local Library = loadfile("Zephyr/GuiLibrary.lua")()
-        shared.GuiLibrary = Library
-
-        local win = Library.CreateWindow({
-            Blur = true,
-            Notify = true,
-            Bind = Enum.KeyCode.V,
-            SelfDestruct = Enum.KeyCode.Insert
-        })
-
-        local combat = win.CreateTab({
-            Name = "Combat"
-        })
-        local visual = win.CreateTab({
-            Name = "Visual"
-        })
-        local movement = win.CreateTab({
-            Name = "Movement"
-        })
-        local exploits = win.CreateTab({
-            Name = "Exploits"
-        })
-        shared.Combat = combat
-        shared.Exploits = exploits
-        shared.Movement = movement
-        shared.Visual = visual
-
-        local Universal = [[
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/IlikeScript1234/Zephyr/main/Universal.lua"))()
-        ]]
-
-        local check = [[
-            shared.Launcher = true
-        ]]
-
-        RFile("Detect1.lua")
-
-        function MainLoaded()
-            local customModuleURL = "https://raw.githubusercontent.com/IlikeScript1234/ZephyrForRobloxV3.0/main/Modules/"..game.PlaceId..".lua"
-            local customModuleScript = game:HttpGet(customModuleURL, true)
-            if customModuleScript then
-                local success, error = pcall(function()
-                    if not isfile("Detect2.lua") then
-                        MakeFile("Modules/"..game.PlaceId, customModuleScript)
-                        writefile("Detect2.lua", check)
-                    end
-                    loadstring(readfile("Zephyr/Modules/".. game.PlaceId .. ".lua"))()
-                end)
-                if not success then
-                    local customModuleWeb = "https://raw.githubusercontent.com/IlikeScript1234/ZephyrForRobloxV3.0/main/Universal.lua"
-                    local customModuleCoding = game:HttpGet(customModuleWeb, true)
-                    warn("Failed To Loaded Modules: " .. tostring(error))
-                    MakeFile("Universal", customModuleCoding)
-                    loadstring(Universal)()
-                end
-            else
-                local customModuleUniversal = "https://raw.githubusercontent.com/IlikeScript1234/ZephyrForRobloxV3.0/main/Universal.lua"
-                local customModuleScriptUniversal = game:HttpGet(customModuleUniversal, true)
-                MakeFile("Universal")
-                loadstring(Universal)()
-            end
-        end
-
-        task.spawn(function()
-            MainLoaded()
-            loadstring(readfile("Detect2.lua"))()
-        end)
-    else
-        loadfile("Zephyr/Modules/".. game.PlaceId.. ".lua")()
     end
+    
+    local MakeFile = function(name, file)
+        if not isfile(name) then
+            writefile("Zephyr/".. name.. ".lua", file)
+        end
+    end
+
+    local RFile = function(name)
+        if isfile(name) then
+            delfile(name)
+        end
+    end
+
+    local execute = function(name)
+        loadfile("Zephyr/".. name .. ".lua")()
+    end
+
+    local check = [[
+        shared.Launcher = true
+    ]]
+
+    local Detect = function(num)
+        writefile("Zephyr/Detect".. num .. ".lua", check)
+    end
+
+    MakeFolder("Zephyr")
+    MakeFolder("Zephyr/Modules")
+    MakeFolder("Zephyr/Assets")
+    MakeFolder("Zephyr/Profiles")
+
+    local UiLibrary = "https://raw.githubusercontent.com/IlikeScript1234/ZephyrForRobloxV3.0/main/GuiLibrary.lua"
+    local GuiC = game:HttpGet(UiLibrary, true)
+    MakeFile("GuiLibrary", GuiC)
+    local Library = loadfile("Zephyr/GuiLibrary.lua")()
+    shared.GuiLibrary = Library
+
+    local win = Library.CreateWindow({
+        Blur = true,
+        Notify = true,
+        Bind = Enum.KeyCode.V,
+        SelfDestruct = Enum.KeyCode.Insert
+    })
+
+    local combat = win.CreateTab({
+        Name = "Combat"
+    })
+    local visual = win.CreateTab({
+        Name = "Visual"
+    })
+    local movement = win.CreateTab({
+        Name = "Movement"
+    })
+    local exploits = win.CreateTab({
+        Name = "Exploits"
+    })
+    shared.Combat = combat
+    shared.Exploits = exploits
+    shared.Movement = movement
+    shared.Visual = visual
+
+
+    RFile("Detect1.lua")
+
+    local LauncherURL = "https://raw.githubusercontent.com/IlikeScript1234/ZephyrForRobloxV3.0/main/Launcher.lua"
+    local LauncherScript = game:HttpGet(LauncherURL, true)
+
+    MakeFile("Launcher", LauncherScript)
+
+    function MainLoaded()
+        local customModuleURL = "https://raw.githubusercontent.com/IlikeScript1234/ZephyrForRobloxV3.0/main/Modules/"local customModuleURL = "https://raw.githubusercontent.com/IlikeScript1234/ZephyrForRobloxV3.0/main/Modules/"..game.PlaceId..".lua"
+        local customModuleScript = game:HttpGet(customModuleURL, true)
+        if customModuleScript then
+            local success, error = pcall(function()
+                if not isfile("Zephyr/Detect2.lua") then
+                    Detect("2")
+                    MakeFile("Modules/"..game.PlaceId, customModuleScript)
+                end
+                execute("Modules/".. game.PlaceId)
+            end)
+            if not success then
+                local customModuleWeb = "https://raw.githubusercontent.com/IlikeScript1234/ZephyrForRobloxV3.0/main/Universal.lua"
+                local customModuleCoding = game:HttpGet(customModuleWeb, true)
+                error("Failed To Loaded Modules: " .. tostring(error))
+                if not isfile("Zephyr/Detect2.lua") then
+                    MakeFile("Universal", customModuleCoding)
+                    
+                end
+            end
+        else
+            local customModuleUniversal = "https://raw.githubusercontent.com/IlikeScript1234/ZephyrForRobloxV3.0/main/Universal.lua"
+            local customModuleScriptUniversal = game:HttpGet(customModuleUniversal, true)
+            if not isfile("Zephyr/Detect2.lua") then
+                MakeFile("Universal")
+                execute("Universal")
+            end
+        end
+    end
+
+    task.spawn(function()
+        MainLoaded()
+        loadstring(readfile("Zephyr/Detect2.lua"))()
+    end)
+else
+    loadfile("Zephyr/Modules/".. game.PlaceId.. ".lua")()
+end
